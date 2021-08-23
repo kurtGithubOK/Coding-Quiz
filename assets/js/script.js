9d457d820f346a66abfbb5275309e6159b377735
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-08-23 11:48:08 -0700

    Got event handlers to be called.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,73 @@
 // GLOBAL VARIABLES ...
 let globalTimer = 0;
+let score = 0;
+const highScores = document.getElementById('high-scores');
+const startButton = document.getElementById('startButton');
+const goBackButton = document.getElementById('goBackButton');
+const clearButton = document.getElementById('clearButton');
+const submitButton = document.getElementById('submit');
 
-// SHOWS ...
-function showStart() {
+highScores.addEventListener('click', viewHighScoresClicked);
+startButton.addEventListener('click', startClicked);
+goBackButton.addEventListener('click', goBackClicked);
+clearButton.addEventListener('click', clearClicked);
+submitButton.addEventListener('click', submitClicked);
+
+
+// SHOWS ... //////////////////////////////////////////////////
+function showStartView() {
     // just turn on divs?
 }
-function showQuestion() {
+function showQuestionView() {
     // get Q from data.
     // Show q and answers
     // show pass/fail thing.
 }
-function showDone() {
+function showDoneView() {
     // ????
 }
-function showHighScores() {
+function showHighScoresView() {
+    console.log('now in showHighScores()')
     // Get list of scores
     // Display list of <li> tags.
     // Show GOBACK & CLEAR buttons
 }
 
-// HANDLERS ...
+
+// HANDLERS ... //////////////////////////////////////////////////
 function startClicked() {
+    console.log('now in startClicked()')
+
     // chnage view to question
 }
 function answerClicked() {
+    console.log('now in answerClicked()')
     // check answer
     // update score
     // if timer > 0 show next question
     // if wrong, decrement timer
     // showDone()
 }
-function initialsEntered() {
+function submitClicked() {
+    console.log('now in submitClicked()')
     // get values
     // save it
     // showHighScores()
 }
 function goBackClicked() {
+    console.log('now in goBackClicked()')
 
 }
 function clearClicked() {
+    console.log('now in clearClicked()')
 
 }
 function viewHighScoresClicked() {
+    console.log('now in viewHighScores()')
     // just showHightscores()
 }
 
-// UTILITIES ...
+// UTILITIES ... //////////////////////////////////////////////////
 function startTimer() {
     // set global timer.
     // setTimeout()
