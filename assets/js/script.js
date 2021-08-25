2a9019a105c6f660e85f1d2891ce63bcff6e2eb5
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-08-25 10:30:14 -0700

    Got answer clicked working.  added more data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,6 @@ const emailFormDiv = document.getElementById('email-form');
 const highScoreListDiv = document.getElementById('high-score-list');
 const goBackClearDiv = document.getElementById('go-back-clear');
 
-
 // Adding event listeners.
 highScoresButton.addEventListener('click', viewHighScoresClicked);
 startButton.addEventListener('click', startClicked);
@@ -48,22 +47,44 @@ let data = {
     ],
     questions: [
         {
-            question: "this is a question from data?",
+            question: "Which of the following is NOT a JS data type?",
             answers: [
-                "yes!",
-                "no?",
-                "how the hell should I know?"
+                "string",
+                "number",
+                "boolean",
+                "apple"
             ],
-            correctAnswer: 1
+            correctAnswer: 3
         },
         {
-            question: "this is question #2!?",
+            question: "Which of the following is a NOT a type of JS loop?",
             answers: [
-                "chicken",
-                "susan burns",
-                "have to cancele somehow?"
+                "loop:",
+                "For",
+                "While",
+                "Do while"
+            ],
+            correctAnswer: 0
+        },
+        {
+            question: "Which is true about JS?",
+            answers: [
+                "Its multi-threaded",
+                "Its only used on the client",
+                "It's amazing",
+                "It is compiled"
             ],
             correctAnswer: 2
+        },
+        {
+            question: "Javscript was developed by ...",
+            answers: [
+                "Microsoft",
+                "Netscape",
+                "Google",
+                "Apple"
+            ],
+            correctAnswer: 1
         }
     ],
     views: {
@@ -80,7 +101,7 @@ function showStartView() {
     updateView('start');
     mainTextDiv.textContent = 'Coding Quiz Challenge';
     subTextDiv.textContent = 'Try to answer the following code-related question within the time limit.' +
-    'Keep in mind that incorrect answers will penalize you score/time by ten seconds!';
+        'Keep in mind that incorrect answers will penalize you score/time by ten seconds!';
 }
 function showQuestionView() {
     updateView('question');
@@ -89,18 +110,25 @@ function showQuestionView() {
     // Show Q&A.
     questionDiv.textContent = questionData.question;
 
+    // Write list of answers to DOM.
     const answers = questionData.answers;
-    let ul = document.createElement('ul');
-    answersDiv.append(ul);
-    for(let answer of answers) {
-        const li = document.createElement('li');
-        // li.textContent = answer;
-        const button = document.createElement("button");
-        button.innerHTML = answer;
-        li.appendChild(button);
-
-        ul.appendChild(li);
+    // If there's a 'ul' from previous question, remove it.
+    if (answersDiv.hasChildNodes()) {
+        answersDiv.removeChild(answersDiv.childNodes[0])
     }
+    answersUl = document.createElement('ul');
+    answersUl.setAttribute('id', 'answer-list');
+    answersDiv.append(answersUl);
+    for (let i = 0; i < answers.length; i++) {
+        const li = document.createElement('li');
+        const button = document.createElement("button");
+        button.innerHTML = answers[i];
+        // Add custom data attribute to be picked up by listener later.
+        button.setAttribute('data-answer-index', i);
+        li.appendChild(button);
+        answersUl.appendChild(li);
+    }
+    answersUl.addEventListener('click', answerClicked);
 }
 function showDoneView() {
     updateView('done');
@@ -124,17 +152,31 @@ function startClicked() {
 
     // chnage view to question
 }
-function answerClicked() {
-    console.log('now in answerClicked()')
-    // check answer
-    // update score
-    // if timer > 0 show next question
-    // if wrong, decrement timer
-    // showDone()
+function answerClicked(event) {
+    const target = event.target;
+    // Only interested in clicked elements if they are a button.
+    if (!target.matches('button')) return;
+
+    // Check answer
+    const userAnswerId = target.attributes.getNamedItem('data-answer-index').value;
+    const correctAnswerId = data.questions[currentQuestionIndex].correctAnswer;
+    if (userAnswerId === correctAnswerId) {
+        score++;
+    } else {
+        globalTimer -= 10;
+    }
+
+    // Are we out of questions?
+    if (currentQuestionIndex === data.questions.length - 1) {
+        showDoneView();
+    } else {
+        currentQuestionIndex++;
+        showQuestionView();
+    }
 }
 function nextQuestionClicked() {
     console.log('now in nextQuestionClicked')
-        // show pass/fail thing.
+    // show pass/fail thing.
 
     showDoneView();
 }
@@ -160,7 +202,7 @@ function viewHighScoresClicked() {
 // UTILITY FUNCTIONS //////////////////////////////////////////////////
 function init() {
     //showStartView();
-showQuestionView();
+    showQuestionView();
 }
 function updateView(targetView) {
     // Turn all divs off.
